888d30de1847d3f9ee4d10d10897ef816e71bf8a
Tris Setiawan <tris.setiawan@yahoo.com>
2016-08-26 07:05:32 +0000

    1.0.0 - fox Promise.chain

--- src/promise-ext.js
+++ src/promise-ext.js
@@ -1,23 +1,20 @@
 function chain(promises) {
-    return new Promise((resolve, reject) => {
-        var results = [];
-        if (promises.length > 0) {
-            promises.reduce((prev, cur, index, arr) => {
-                    return Promise.resolve(prev)
-                        .then(result => {
-                            results.push(result);
-                            return Promise.resolve(cur);
-                        });
-                }, null)
-                .then(r => {
-                    results.push(r);
-                    results.splice(0, 1);
-                    resolve(results);
-                });
-        }
-        else
-            resolve([]);
-    });
+    var results = [];
+    if (promises && promises.length > 0) {
+        return promises.reduce((prev, cur, index, arr) => {
+                return prev.then(function(result) {
+                    results.push(result);
+                    return Promise.resolve(cur());
+                })
+            }, Promise.resolve(null))
+            .then(result => {
+                results.push(result);
+                results.splice(0, 1);
+                return results;
+            })
+    }
+    else
+        return Promise.resolve([]);
 }
 
 
